1bfaae921bafb78c0522d3e330c3f1475828a9b5
wolfsflowgmai <luthier.wolf@yahoo.com.br>
2019-06-05 15:48:00 -0300

    created a posting method in api and requirements.txt file for setup.

--- cadastro/api/views.py
+++ cadastro/api/views.py
@@ -1,5 +1,5 @@
 
-from rest_framework import generics
+from rest_framework import generics, mixins
 from cadastro.models import Funcionario
 from .serializer import FuncionarioSerializer
 
@@ -8,11 +8,15 @@ class FuncionarioCrudApiView(generics.RetrieveUpdateDestroyAPIView):
 	look_up_field = "pk"
 	serializer_class = FuncionarioSerializer
 	def get_queryset(self):
-		return Funcionario
+		return Funcionario.objects.all()
 
-class FuncionarioApiView(generics.CreateAPIView):
+
+class FuncionarioApiView(mixins.CreateModelMixin, generics.ListAPIView):
 	
 	look_up_field = "pk"
 	serializer_class = FuncionarioSerializer
 	def get_queryset(self):
-		return Funcionario
+		return Funcionario.objects.all()
+
+	def post(self, request, *args, **kwargs):
+		return self.create(request, *args, *kwargs)
--- luizalab/urls.py
+++ luizalab/urls.py
@@ -20,5 +20,5 @@ from cadastro.views import home_view
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', home_view, name='home'),
-    path('api/cadastro', include('cadastro.api.urls')),
+    path('api/cadastro/', include('cadastro.api.urls')),
 ]
